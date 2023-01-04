62a765c2a60db051d0c675ad787b961d20237f31
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-04 21:09:46 +0100

    Fixed Formula task formula

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,6 +3,7 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
         short a, b;
-        String formula = "(9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2";
+        a = b = 0;
+        int formula = (9 * (a ^ 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
